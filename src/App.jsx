1aaa8e5463a50e0f10a4cac9586cf5120253f4ea
Tetiana Naumenko <naumenko.ta@gmail.com>
2025-10-16 13:13:29 +0300

    add petBlock

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,28 @@ import LoginForm from "./components/LoginForm/LoginForm";
 import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
 import MainPage from "./pages/MainPage/MainPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import PetBlock from "./components/PetBlock/PetBlock";
+
+import mob1x from "./assets/images/login/login-mob.webp";
+import mob2x from "./assets/images/login/login2x-mob.webp";
+import tab1x from "./assets/images/login/login-tab.webp";
+import tab2x from "./assets/images/login/login2x-tab.webp";
+import desk1x from "./assets/images/login/login.webp";
+import desk2x from "./assets/images/login/login2x.webp";
 
 function App() {
   return (
     <>
       <Toaster position="top-center" reverseOrder={false} />
       <Container>
+        <PetBlock
+          mob1x={mob1x}
+          mob2x={mob2x}
+          tab1x={tab1x}
+          tab2x={tab2x}
+          desk1x={desk1x}
+          desk2x={desk2x}
+        />
         <RegistrationForm />
         <LoginForm />
       </Container>
---
+++ src/components/PetBlock/PetBlock.jsx
@@ -0,0 +1,31 @@
+import s from "./PetBlock.module.css";
+
+export default function PetBlock({
+  mob1x,
+  mob2x,
+  tab1x,
+  tab2x,
+  desk1x,
+  desk2x,
+}) {
+  return (
+    <div className={s.wrapper}>
+      <picture>
+        <source
+          media="(min-width: 1280px)"
+          srcSet={`${desk1x} 1x, ${desk2x} 2x`}
+        />
+        <source
+          media="(min-width: 768px)"
+          srcSet={`${tab1x} 1x, ${tab2x} 2x`}
+        />
+        <source
+          media="(max-width: 767px)"
+          srcSet={`${mob1x} 1x, ${mob2x} 2x`}
+        />
+
+        <img src={desk1x} alt="Pet" />
+      </picture>
+    </div>
+  );
+}
